0edf3a268b2e310cebda99b63843d8f3789f72cf
Arthur Sakemi <arthursakemi@gmail.com>
2019-09-11 12:12:28 -0300

    código refatorado para melhor legibilidade

--- centroGravidadeMatriz/src/centrogravidadematriz/Main.java
+++ centroGravidadeMatriz/src/centrogravidadematriz/Main.java
@@ -9,45 +9,124 @@ public class Main {
         double[][] input = input();
         int[] centro;
 
-        printVetor(somaLinhas(input));
-        System.out.println("");
-        printVetor(somaColunas(input));
-        System.out.println("");
-
         centro = centroGravidade(input);
 
-        for (int i = 0; i < 2; i++) {
-            System.out.println(centro[i] + " ");
-        }
+        System.out.printf("Centro: (%d, %d)\n", centro[0], centro[1]);
 
     }
 
     public static double[][] input() throws Exception {
-        BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+        String[][] inputString;
+        double[][] input;
 
-        String[] tString = br.readLine().split(" ");
-        int[] t = new int[2];
+        inputString = inputString();
+        input = inputConvert(inputString);
 
-        for (int i = 0; i < 2; i++) {
-            t[i] = Integer.parseInt(tString[i]);
-        }
+        return input;
+    }
 
-        String[][] matrizString = new String[t[0]][t[1]];
-        double[][] input = new double[matrizString.length][matrizString[0].length];
+    public static double[][] inputConvert(String[][] matriz) {
+        double[][] input = new double[matriz.length][matriz[0].length];
 
-        for (int i = 0; i < matrizString.length; i++) {
-            matrizString[i] = br.readLine().split(" ");
-        }
-
-        for (int i = 0; i < matrizString.length; i++) {
-            for (int j = 0; j < matrizString[i].length; j++) {
-                input[i][j] = Double.parseDouble(matrizString[i][j]);
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                input[i][j] = Double.parseDouble(matriz[i][j]);
             }
         }
 
         return input;
     }
 
+    public static String[][] inputString() throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+        String[][] matriz;
+        String[] tString;
+        int[] t = new int[2];
+
+        tString = br.readLine().split(" ");
+
+        for (int i = 0; i < 2; i++) {
+            t[i] = Integer.parseInt(tString[i]);
+        }
+
+        matriz = new String[t[0]][];
+
+        for (int i = 0; i < matriz.length; i++) {
+            matriz[i] = br.readLine().split(" ");
+        }
+        return matriz;
+    }
+
+    public static int[] centroGravidade(double[][] matriz) {
+        int[] centro = new int[2];
+        centro[0] = linhaCentro(somaLinhas(matriz)) + 1;
+        centro[1] = linhaCentro(somaColunas(matriz)) + 1;
+        return centro;
+    }
+
+    //descobre qual a linha ou coluna que é o centro de gravidade
+    public static int linhaCentro(double[] linha) {
+        int x = 0;
+        double temp;
+        double diferenca = 0;
+        double superior;
+        double inferior;
+
+        //loop externo demarca qual das linhas válidas para o centro de gravidade
+        //está sendo avaliada;
+        for (int i = 1; i < linha.length - 1; i++) {
+            superior = 0;
+            inferior = 0;
+
+            //loop interno separa e soma as linhas em seus grupos apropriados
+            for (int j = 0; j < linha.length; j++) {
+                if (j < i) {
+                    superior += linha[j];
+                }
+                if (j > i) {
+                    inferior += linha[j];
+                }
+            }
+
+            temp = Math.abs(superior - inferior);
+
+            //checa se a diferença atual das partes é a menor possível nesse momento
+            if (i == 1) {
+                diferenca = temp;
+            } else if (temp < diferenca) {
+                diferenca = temp;
+                x = i;
+            }
+
+        }
+        return x;
+    }
+
+    //soma todas as linhas da matriz e as armazena em um vetor
+    public static double[] somaLinhas(double[][] matriz) {
+        double[] somaLinhas = new double[matriz.length];
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                somaLinhas[i] += matriz[i][j];
+            }
+        }
+        return somaLinhas;
+    }
+
+    //soma todas as colunas da matriz e as armazena em um vetor
+    public static double[] somaColunas(double[][] matriz) {
+        double[] somaColunas = new double[matriz[0].length];
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                somaColunas[j] += matriz[i][j];
+            }
+        }
+        return somaColunas;
+    }
+
+    //funções de print usadas para testar as outras funções
     public static void print(double[][] matriz) {
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
@@ -61,66 +140,6 @@ public class Main {
         for (int i = 0; i < vetor.length; i++) {
             System.out.printf("%.1f ", vetor[i]);
         }
-    }
-
-    public static double[] somaLinhas(double[][] matriz) {
-        double[] somaLinhas = new double[matriz.length];
-
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                somaLinhas[i] += matriz[i][j];
-            }
-        }
-        return somaLinhas;
-    }
-
-    public static double[] somaColunas(double[][] matriz) {
-        double[] somaColunas = new double[matriz[0].length];
-
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                somaColunas[j] += matriz[i][j];
-            }
-        }
-        return somaColunas;
-    }
-
-    public static int[] centroGravidade(double[][] matriz) {
-        int[] centro = new int[2];
-        centro[0] = linhaCentro(somaLinhas(matriz));
-        centro[1] = linhaCentro(somaColunas(matriz));
-        return centro;
-    }
-
-    public static int linhaCentro(double[] linha) {
-        int x = 0;
-        double temp;
-        double diferenca = 0;
-        double superior;
-        double inferior;
-
-        for (int i = 1; i < linha.length - 1; i++) {
-            superior = 0;
-            inferior = 0;
-            for (int j = 0; j < linha.length; j++) {
-                if (j < i) {
-                    superior += linha[j];
-                }
-                if (j > i) {
-                    inferior += linha[j];
-                }
-            }
-            temp = superior > inferior ? superior - inferior : inferior - superior;
-
-            if (i == 1) {
-                diferenca = temp;
-            } else if (temp < diferenca) {
-                diferenca = temp;
-                x = i + 1;
-            }
-
-        }
-        return x;
     }
 
 }
